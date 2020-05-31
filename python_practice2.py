2977e564d64b98600359704bbb07f87c1e129e34
Michelle Piwowarski <mpiwow@Michelles-MacBook-Pro.local>
2020-05-31 16:55:02 -0700

    Adding PyPoll.py and other files.

---
+++ python_practice2.py
@@ -0,0 +1,36 @@
+#counties = ["Arapahoe","Denver","Jefferson"]
+#if "Arapahoe" in counties and "El Paso" not in counties:
+#   print("Only Arapahoe is in the list of counties.")
+#else:
+#    print("Arapahoe is in the list of counties and El Paso is not in the list of counties.")
+
+#x = 0
+#while x <= 5:
+ #   print(x)
+ #   x = x + 1
+
+#numbers = [0, 1, 2, 3, 4]
+#for num in numbers:
+ #   print(num)
+
+#counties_dict = {"Arapahoe": 422829, "Denver": 463353, "Jefferson": 432438}
+#for county in counties_dict:
+#    print(county)     #REPLACING THE QUERY BELOW SHOULD GET THE SAME OUTPUT
+
+#counties_dict = {"Arapahoe": 422829, "Denver": 463353, "Jefferson": 432438}
+#for county in counties_dict.keys():
+#    print(county)
+
+#voting_data = [{"county":"Arapahoe", "registered_voters": 422829},
+   #             {"county":"Denver", "registered_voters": 463353},
+    #            {"county":"Jefferson", "registered_voters": 432438}]
+
+#for county_dict in voting_data:
+ #   for value in county_dict.values():
+  #      print(value)
+
+#my_votes = int(input("How many votes did you get in the election? "))
+#total_votes = int(input("What is the total votes in the election? "))
+#print(f"I received {my_votes / total_votes * 100}% of the total votes.")
+
+counties_dict = {"Arapahoe": 422829, "Denver": 463353, "Jefferson": 432438}
